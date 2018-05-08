5f1f5e0e840331ad2a729673b0c586c9f64670ac
Will Neville <william@neville.com>
2018-05-08 19:31:03 -0400

    Creating lein project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject peatsheep/handsome-hunter "0.0.1"
+  :description "Diversify your Bloodborne runs"
+  :url "https://github.com/peatsheep/handsome-hunter"
+  :license {:name "GNU General Public License version 3"
+            :url "https://opensource.org/licenses/GPL-3.0"}
+  :dependences [[org.clojure/clojure "1.8.0"]])
